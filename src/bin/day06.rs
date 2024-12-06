5d74600a530019939a9fa141db28d5fbf703da4e
StygianLightning <denrellum@gmail.com>
2024-12-06 18:19:19 +0100

    Day 6 part 1.

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,15 +1,17 @@
+use std::collections::{HashMap, HashSet};
+
 use aoc2024::{
     grid::Grid,
-    index2::{uidx2, UIndex2},
+    index2::{uidx2, Index2, UIndex2},
 };
 
-#[derive(Debug, Copy, Clone, Default)]
+#[derive(Debug, Copy, Clone, Default, Eq, PartialEq)]
 enum Tile {
     #[default]
     Empty,
     Blocked,
 }
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 enum Direction {
     Up,
     Down,
@@ -23,6 +25,44 @@ struct Guard {
     position: UIndex2,
 }
 
+impl Direction {
+    fn to_index2(self) -> Index2 {
+        match self {
+            Direction::Up => Index2 { x: 0, y: -1 },
+            Direction::Down => Index2 { x: 0, y: 1 },
+            Direction::Left => Index2 { x: -1, y: 0 },
+            Direction::Right => Index2 { x: 1, y: 0 },
+        }
+    }
+
+    fn turn_clockwise(self) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
+        }
+    }
+}
+
+impl Guard {
+    fn next_position(&self, grid: &Grid<Tile>) -> Option<UIndex2> {
+        let offset = self.direction.to_index2();
+        let target = self.position.to_index2() + offset;
+
+        if target.x < 0 || target.y < 0 {
+            return None;
+        }
+
+        let idx = uidx2(target.x as _, target.y as _);
+        if grid.get(idx).is_some() {
+            Some(idx)
+        } else {
+            None
+        }
+    }
+}
+
 fn parse(text: &str) -> (Grid<Tile>, Guard) {
     let num_lines = text.lines().count();
     let num_per_line = text.lines().next().unwrap().len();
@@ -33,7 +73,7 @@ fn parse(text: &str) -> (Grid<Tile>, Guard) {
 
     for (line_idx, line) in text.lines().enumerate() {
         for (idx, c) in line.char_indices() {
-            let idx = uidx2(line_idx as _, idx as _);
+            let idx = uidx2(idx as _, line_idx as _);
 
             grid[idx] = match c {
                 '#' => Tile::Blocked,
@@ -58,7 +98,55 @@ fn parse(text: &str) -> (Grid<Tile>, Guard) {
 
 fn main() {
     let input = std::fs::read_to_string("input/day06.txt").unwrap();
+
     let (grid, guard) = parse(&input);
 
-    println!("{guard:?}");
+    let part1_res = part1(&grid, guard);
+
+    println!("part 1 result: {part1_res}");
+}
+
+fn part1(grid: &Grid<Tile>, mut guard: Guard) -> u32 {
+    let mut seen: HashSet<(UIndex2, Direction)> = HashSet::new();
+
+    loop {
+        if seen.contains(&(guard.position, guard.direction)) {
+            break;
+        }
+        seen.insert((guard.position, guard.direction));
+
+        let Some(next_position) = guard.next_position(grid) else {
+            break;
+        };
+        if grid[next_position] == Tile::Blocked {
+            guard.direction = guard.direction.turn_clockwise();
+        } else {
+            guard.position = next_position;
+        }
+    }
+
+    // count all seen squares, but ignore multiple visits from different directions
+    let all_visited = seen
+        .iter()
+        .map(|(position, _)| *position)
+        .collect::<HashSet<_>>();
+
+    for y in 0..grid.dimension().y {
+        for x in 0..grid.dimension().x {
+            let idx = uidx2(x, y);
+
+            if all_visited.contains(&idx) {
+                print!("X");
+            } else {
+                match grid[idx] {
+                    Tile::Empty => print!("."),
+                    Tile::Blocked => print!("#"),
+                }
+            }
+        }
+        println!();
+    }
+    println!();
+
+    all_visited.len() as _
 }
