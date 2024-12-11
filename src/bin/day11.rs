16a82d2130b71b1683ce22397fc91a98e86a5fb2
StygianLightning <denrellum@gmail.com>
2024-12-11 18:07:32 +0100

    Day 11 part 2.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,33 +1,40 @@
-fn part1(nums: &[u32]) -> u32 {
-    // naive, no memoization
+use std::collections::HashMap;
 
-    nums.iter()
-        .cloned()
-        .map(|num| {
-            let mut current = vec![num as u64];
-            let mut next = vec![];
-            for _ in 0..25 {
-                for x in current.iter().cloned() {
-                    if x == 0 {
-                        next.push(1);
-                    } else if x.ilog10() % 2 == 1 {
-                        let num_digits = x.ilog10() + 1;
-                        let separator = u64::pow(10, num_digits / 2);
-                        let right = x % separator;
-                        let left = x / separator;
-                        next.push(left);
-                        next.push(right);
-                    } else {
-                        next.push(x * 2024);
-                    }
-                }
-                std::mem::swap(&mut current, &mut next);
-                next.clear();
+fn blink(nums: &[u32], num_iterations: u32) -> u64 {
+    let mut current = HashMap::new();
+
+    for num in nums {
+        current.entry(*num as u64).or_insert(1);
+    }
+    let mut next = HashMap::new();
+
+    for _ in 0..num_iterations {
+        for (x, amount) in current.iter() {
+            if *x == 0 {
+                let successor = 1;
+                let successors = next.entry(successor).or_default();
+                *successors += *amount;
+            } else if x.ilog10() % 2 == 1 {
+                let num_digits = x.ilog10() + 1;
+                let separator = u64::pow(10, num_digits / 2);
+                let left = *x / separator;
+                let right = *x % separator;
+
+                let left = next.entry(left).or_default();
+                *left += *amount;
+                let right = next.entry(right).or_default();
+                *right += *amount;
+            } else {
+                let successor = x * 2024;
+                let successors = next.entry(successor).or_default();
+                *successors += *amount;
             }
+        }
+        std::mem::swap(&mut current, &mut next);
+        next.clear();
+    }
 
-            current.len() as u32
-        })
-        .sum()
+    current.values().sum()
 }
 
 fn main() {
@@ -37,6 +44,9 @@ fn main() {
         .map(|num| num.parse().unwrap())
         .collect::<Vec<_>>();
 
-    let part1_res = part1(&nums);
+    let part1_res = blink(&nums, 25);
     println!("part 1: {part1_res}");
+
+    let part2_res = blink(&nums, 75);
+    println!("part 2: {part2_res}");
 }
