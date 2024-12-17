54eeed052a5b0478643f3789ebd1a52487099545
StygianLightning <denrellum@gmail.com>
2024-12-17 18:30:34 +0100

    Day 16 parse input.

---
+++ src/bin/day16.rs
@@ -0,0 +1,74 @@
+use aoc2024::{
+    direction::Direction,
+    grid::Grid,
+    index2::{uidx2, UIndex2},
+};
+
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+enum Tile {
+    #[default]
+    Empty,
+    Wall,
+}
+
+#[derive(Debug)]
+struct Input {
+    start: UIndex2,
+    end: UIndex2,
+    grid: Grid<Tile>,
+}
+
+#[derive(Debug)]
+struct State {
+    position: UIndex2,
+    facing: Direction,
+}
+
+impl State {
+    fn new(position: UIndex2) -> Self {
+        Self {
+            position,
+            facing: Direction::Left,
+        }
+    }
+}
+
+fn parse(input: &str) -> Input {
+    let width = input.lines().next().unwrap().len() as u32;
+    let height = input.lines().take_while(|l| !l.trim().is_empty()).count() as u32;
+    let size = uidx2(width, height);
+
+    let mut grid = Grid::new_with_default(size);
+    let mut start = uidx2(0, 0);
+    let mut end = uidx2(0, 0);
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.char_indices() {
+            let idx = uidx2(x as u32, y as u32);
+            grid[idx] = match c {
+                '#' => Tile::Wall,
+                '.' => Tile::Empty,
+                'S' => {
+                    start = idx;
+
+                    Tile::Empty
+                }
+                'E' => {
+                    end = idx;
+                    Tile::Empty
+                }
+                _ => panic!("unknown tile {c}"),
+            };
+        }
+    }
+
+    Input { grid, start, end }
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input/day16.txt").unwrap();
+    let input = parse(&input);
+    println!("{input:#?}");
+    let state = State::new(input.start);
+    println!("{state:#?}");
+}
