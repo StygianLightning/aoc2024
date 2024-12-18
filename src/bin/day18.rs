9827c14f203e50a6e988e6a8aa0042ec718d5f95
StygianLightning <denrellum@gmail.com>
2024-12-18 14:46:47 +0100

    Day 18 part 2.

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -25,42 +25,40 @@ enum Tile {
     Blocked,
 }
 
-fn part1(indices: &[UIndex2], grid_size: UIndex2) -> u32 {
-    let mut grid = Grid::new_with_default(grid_size);
-
+fn flood_fill(indices: &[UIndex2], grid: &mut Grid<Tile>) -> Result<u32, ()> {
     for idx in indices.iter().cloned() {
         grid[idx] = Tile::Blocked;
     }
 
     let start = uidx2(0, 0);
-    let target = grid_size - uidx2(1, 1);
+    let target = grid.dimension() - uidx2(1, 1);
 
     let mut open = vec![start];
     let mut next = vec![];
-
-    // we could also not save the grid and just put all the blocked indices in `visited`. Let's wait for the part 2 twist first.
-    let mut visited = HashSet::new();
-    visited.insert(start);
 
     let mut ret = 0;
 
     loop {
         while let Some(idx) = open.pop() {
             if idx == target {
-                return ret;
+                return Ok(ret);
             }
             for d in Direction::ALL {
                 let Some(idx) = d.get_neighbor(idx, &grid) else {
                     continue;
                 };
 
-                if visited.contains(&idx) || grid[idx] == Tile::Blocked {
+                if grid[idx] == Tile::Blocked {
                     continue;
                 }
 
                 next.push(idx);
-                visited.insert(idx);
+                grid[idx] = Tile::Blocked; // no walk back
             }
+        }
+        if next.is_empty() {
+            // flood fill failed
+            return Err(());
         }
         ret += 1;
         std::mem::swap(&mut open, &mut next);
@@ -68,18 +66,32 @@ fn part1(indices: &[UIndex2], grid_size: UIndex2) -> u32 {
     }
 }
 
+fn part2(indices: &[UIndex2], grid: &mut Grid<Tile>) -> usize {
+    (1..=indices.len())
+        .collect::<Vec<_>>()
+        .partition_point(|i| {
+            grid.reset_to_default();
+            flood_fill(&indices[..*i], grid).is_ok()
+        })
+}
+
 fn main() {
     let input = std::fs::read_to_string("input/day18.txt").unwrap();
-    let input = std::fs::read_to_string("input/test_day18.txt").unwrap();
 
     let grid_size = uidx2(71, 71);
-    let grid_size = uidx2(7, 7);
 
     let indices = parse(&input);
 
-    let part1_limit = 1024;
-    let part1_limit = 12;
+    let mut grid = Grid::new_with_default(grid_size);
 
-    let part1_res = part1(&indices[..part1_limit], grid_size);
+    let part1_limit = 1024;
+    let part1_res = flood_fill(&indices[..part1_limit], &mut grid).unwrap();
     println!("part 1 result: {part1_res}");
+
+    let part2_res = part2(&indices, &mut grid);
+    let blocking_element = indices[part2_res];
+    println!(
+        "part 2 result: {},{}",
+        blocking_element.x, blocking_element.y
+    );
 }
