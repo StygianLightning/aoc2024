8f3a0c34f73240dc8e7a9dc5c27b795a6e80e306
StygianLightning <denrellum@gmail.com>
2024-12-20 06:51:15 +0100

    Day 20 part 1.

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use aoc2024::{
+    direction::Direction,
     grid::Grid,
     index2::{uidx2, UIndex2},
 };
@@ -72,8 +75,71 @@ fn parse(input: &str) -> Input {
     Input { grid, start, end }
 }
 
+fn shortcut_savings(input: &Input) -> HashMap<u32, u32> {
+    let mut distance_from_start = HashMap::new();
+    distance_from_start.insert(input.start, 0);
+
+    let mut next = vec![input.start];
+    let mut path = vec![];
+
+    while let Some(node) = next.pop() {
+        path.push(node);
+        for direction in Direction::ALL {
+            if let Some(neighbor) = direction.get_neighbor(node, &input.grid) {
+                if input.grid[neighbor] == Tile::Empty
+                    && !distance_from_start.contains_key(&neighbor)
+                {
+                    next.push(neighbor);
+                    distance_from_start.insert(neighbor, distance_from_start[&node] + 1);
+                }
+            }
+        }
+    }
+
+    let mut shortcut_savings = HashMap::new();
+
+    for node in path.iter().cloned() {
+        for direction in Direction::ALL {
+            if let Some(neighbor) = direction.get_neighbor(node, &input.grid) {
+                if input.grid[neighbor] != Tile::Wall {
+                    continue;
+                }
+                let Some(next_in_direction) = direction.get_neighbor(neighbor, &input.grid) else {
+                    continue;
+                };
+                if input.grid[next_in_direction] != Tile::Empty {
+                    continue;
+                }
+
+                if distance_from_start[&next_in_direction] < distance_from_start[&node] {
+                    // going backwards
+                    continue;
+                }
+
+                let savings =
+                    distance_from_start[&next_in_direction] - distance_from_start[&node] - 2;
+                let count = shortcut_savings.entry(savings).or_default();
+                *count += 1;
+            }
+        }
+    }
+
+    shortcut_savings
+}
+
+fn part1(input: &Input) -> u32 {
+    let shortcut_savings = shortcut_savings(input);
+    shortcut_savings
+        .iter()
+        .filter_map(|(k, v)| (*k >= 100).then_some(*v))
+        .sum()
+}
+
 fn main() {
     let input = std::fs::read_to_string("input/day20.txt").unwrap();
     let input = parse(&input);
     input.print();
+
+    let part1 = part1(&input);
+    println!("part 1: {part1}");
 }
